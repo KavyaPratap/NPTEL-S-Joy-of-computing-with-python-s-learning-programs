5de3abf7e8069d878c0d2162fbd6bd4f1d3fd69a
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2024-08-24 17:48:09 +0530

    Update magicsquare.py

--- magicsquare.py
+++ magicsquare.py
@@ -1,43 +1,47 @@
-def kp(n):
-  #initializing matrix of order nxn initially filled with zeroes..(0)
-  ms=[]
-  for i in range(n):
-    l=[]
-    for j in range(n):
-      l.append(0)
-    ms.append(l)
-
-  i=n//2 # i is initialized to the middle row
-  j=n-1 # j is initialized to the last column
-
-  # placing numbers from 1 to n^2 
+def magic_square(n):
+    # Initialize an n x n matrix with zeros
+    magicsquare = []
+    for i in range(n):
+        l = []
+        for j in range(n):
+            l.append(0)
+        magicsquare.append(l)
   
-  num=n*n 
-  count=1 
+    # Start position
+    i = n // 2
+    j = n - 1
   
-  while (count<=num):
-    if (i==-1 and j==n):
-      i=0
-      j=n-2
-    else:
-      if (j==n):
-        j=0
-      if (i<0):
-        i=n-1
+    # Fill the matrix with numbers from 1 to n*n
+    num = n * n 
+    count = 1 
+  
+    while count <= num:
+        # Wrap around if out of bounds
+        if i == -1 and j == n:
+            i = 0
+            j = n - 2
+        else:
+            if j == n:
+                j = 0
+            if i < 0:
+                i = n - 1
         
-    if (ms[i][j]!=0) :
-      j=j-2
-      i=i+1 
-      continue
-    else:
-      ms[i][j]=count
-      count+=1 
-    i=i-1
-    j=j+1 
-    # printing the magic square...
-  for i in range(n):
-    for j in range(n):
-      print(ms[i][j],end=" ")
-    print()
+        # If cell is already filled, move to the next position
+        if magicsquare[i][j] != 0:
+            j = j - 2
+            i = i + 1 
+            continue
+        else:
+            magicsquare[i][j] = count
+            count += 1 
+        i = i - 1
+        j = j + 1 
+    
+    # Print the magic square
+    for i in range(n):
+        for j in range(n):
+            print(magicsquare[i][j], end=" ")
+        print()
 
-kp(5)
+# Example usage
+magic_square(5)
